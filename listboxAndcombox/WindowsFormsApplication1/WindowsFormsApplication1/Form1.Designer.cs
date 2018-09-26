6ce799435cae9a133618f106a9998bf2e884e564
jiesen <f23939889@yahoo.com.tw>
2018-09-26 11:28:30 +0800

    comboBox會顯示WindowsFormsApplication1.Form1+Menu

---
+++ listboxAndcombox/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(377, 109);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "刪除";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(219, 83);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 232);
+            this.listBox1.TabIndex = 1;
+            
+            this.listBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseClick);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 42);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(292, 20);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(681, 339);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ listboxAndcombox/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    { 
+        Boolean showMsg = true;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        class Menu
+        {
+            public String bentoName { get; set; }
+            public int bentoPrice { get; set; }
+            public int OrderQty { get; set; }
+            public int TPrice { get; set; }
+
+
+        }
+        private void upcomboBox()
+        {
+            comboBox1.DataSource = null;
+            comboBox1.DataSource = newmenu;
+            comboBox1.DisplayMember = "bentoName";
+            comboBox1.ValueMember = "bentoName";
+        }
+        private void uplistBox()
+        {
+            listBox1.DataSource = null;
+            listBox1.DataSource = newmenu;
+            listBox1.DisplayMember = "bentoName";
+        }
+        List<Menu> newmenu = new List<Menu>();
+
+        private void selectData()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                newmenu.Add(new Menu() { bentoName = "測試" + i, bentoPrice = 100 });
+            }
+                 
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            selectData();//list塞 newmenu.Add(new Menu() { bentoName = "測試" + i, bentoPrice = 100 });
+            uplistBox();//list塞進listBox 並做設定讓listBox顯示 bentoName的值
+            upcomboBox();//list塞進comboBox並做設定comboBox顯示 bentoName的值
+         
+        }
+        int listBoxRemoveIndex;
+       
+        private void button1_Click(object sender, EventArgs e)//刪除扭
+        {
+            newmenu.RemoveAt(listBoxRemoveIndex);
+            uplistBox();           
+        }
+
+        private void listBox1_MouseClick(object sender, MouseEventArgs e)
+        {          
+            listBoxRemoveIndex = listBox1.IndexFromPoint(e.X, e.Y);           
+            comboBox1.DataSource = null;
+            comboBox1.DataSource = newmenu;
+            comboBox1.DisplayMember = "bentoName";
+            comboBox1.SelectedIndex = listBoxRemoveIndex;
+        }
+
+ 
+    }
+}
